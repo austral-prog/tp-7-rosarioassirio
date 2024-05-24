1bfac2b442db7d0ea8803190faf7985cd2eb1595
rosarioassirio <162721687+rosarioassirio@users.noreply.github.com>
2024-05-23 23:16:54 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,36 @@
-def index_of_by_index(word, list, index):
-    return -1
-
+def index_of_by_index (word, list, index):
+    if word in list [index:]:
+        return list.index(word, index)
+    else:
+        return -1
 
 def index_of_empty(list):
-    return -1
-
+      if ("") in list:
+        return list.index("")
+    else:
+        return -1
 
 def index_of(word, list):
-    return -1
-
+   if word in list:
+        return list.index(word)
+    else:
+        return -1
 
 def put(word, list):
-    return -1
-
+     if "" in list:
+        pos = list.index("")
+        list[pos] = word
+        return pos
+    else:
+        return -1
 
 def remove(word, list):
-    return -1
+    num = 0
+    if word in list:
+        for i in range(len(list)):
+            if list[i] == word:
+                list[i] = ""
+                num += 1
+        return num
+    else:
+        return 0
