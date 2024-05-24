de705835852424f98e4f31a06119545615784823
rosarioassirio <162721687+rosarioassirio@users.noreply.github.com>
2024-05-23 23:36:44 -0300

    1 loops.py

--- loops.py
+++ loops.py
@@ -1,40 +1,32 @@
 def index_of_by_index (word, list, index):
-    if word in list[index:]:
-        return list.index(word, index)
-    else:
-        return -1
-
+  for i in range(index, len(list)):
+        if list[i] == word:
+            return i
+    return -1
 
 def index_of_empty(list):
-      if ("") in list:
-        return list.index("")
-    else:
-        return -1
-
+    for i, index in enumerate(list):
+        if index == "":
+            return i
+    return -1
 
 def index_of(word, list):
-   if word in list:
-        return list.index(word)
-    else:
-        return -1
-
+    for i, index in enumerate(list):
+        if index == word:
+            return i
+    return -1
 
 def put(word, list):
-     if "" in list:
-        pos= list.index("")
-        list[pos] = word
-        return pos
-    else:
-        return -1
-
+    for i in range(len(list)):
+        if list[i] == "":
+            list[i] = word
+            return i
+    return -1
 
 def remove(word, list):
-    num= 0
-    if word in list:
-        for i in range(len(list)):
-            if list[i] == word:
-                list[i] = ""
-                num += 1
-        return num
-    else:
-        return 0
+    eliminate = 0
+    for i in range(len(list)):
+        if list[i] == word:
+            list[i] = ""
+            eliminate += 1
+    return eliminate
