c93e8a0a4f0784de313df5f9939b155b2fb5ad50
rosarioassirio <162721687+rosarioassirio@users.noreply.github.com>
2024-05-23 23:39:40 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,32 +1,39 @@
-def index_of_by_index (word, list, index):
-  for i in range(index, len(list)):
-        if list[i] == word:
-            return i
-    return -1
+def index_of_by_index(x,y,z):
+    index = -1
+    if x in y[z::]:
+        index = y[z::].index(x)
+        index = index+z
+    return index
+index_of_by_index("Magenta",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black", "Green", "Brown", "Red", "Black"], 1)
 
-def index_of_empty(list):
-    for i, index in enumerate(list):
-        if index == "":
-            return i
-    return -1
 
-def index_of(word, list):
-    for i, index in enumerate(list):
-        if index == word:
-            return i
+def index_of_empty(colors):
+    for element in colors:
+        if not element:
+            return colors.index(element)
     return -1
+index_of_empty(["Red", "Green", "", "", "Pink", "", "Black"])
 
-def put(word, list):
-    for i in range(len(list)):
-        if list[i] == "":
-            list[i] = word
-            return i
-    return -1
 
-def remove(word, list):
-    eliminate = 0
-    for i in range(len(list)):
-        if list[i] == word:
-            list[i] = ""
-            eliminate += 1
-    return eliminate
+def index_of(x,y):
+    index = -1
+    if x in y:
+        index = y.index(x)
+    return index
+index_of("Black",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"])
+
+def put(x, list):
+    if index_of_empty(list) != -1:
+        list[index_of_empty(list)] = x
+        return list.index(x)
+    else:
+        return -1
+put("Blue",["Red", "Green", "", "", "Pink", "", "Black"])
+
+
+def remove(x, list):
+    a = list.count(x)
+    while x in list:
+        list[list.index(x)] = ""
+    return a
+remove("Blue",["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"])
